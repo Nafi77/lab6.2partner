adf5ec69a52cab12d84f1cc9ffd9154b116143c0
Miq <97996650+SMiqD@users.noreply.github.com>
2023-10-31 16:44:01 -0400

    Syed Miqdad: decode function added

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ def encode(password):
         encoded_digit = (int(digit) + 3) % 10
         encoded += str(encoded_digit)
     return encoded
+#Syed Miqdad Contribution
+def decode(encoded):
+    decoded_password = ""
+    for i in encoded:
+        i = int(i)
+        if 0 <= i <= 3:
+            if i == 0:
+                i = "7"
+            if i == 1:
+                i = "8"
+            if i == 2:
+                i = "9"
+            decoded_password += i
+        else:
+            i -= 3
+            i = str(i)
+            decoded_password += i
+    return decoded_password
+#Syed Contribution end 
 #I just used a simple for loop to go through each digit and encode it
 def print_menu():
     return (f"Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
@@ -29,6 +48,7 @@ while value == True:
         print(f"The encoded password is {encoded}, and the original password is {ec}")
         print(f'\n{print_menu()}')
         menu_option = int(input("Please enter an option: "))
+        print(decode(encoded_password))
     elif menu_option == 3:
         value = False
 #This uses a while loop to go through the first 3 options
